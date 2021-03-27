6cbe3fb873900eaaccd26873f8348b2bc98a4576
Ryukazu <ryukazuas@gmail.com>
2021-03-27 12:26:17 +0700

    initial commit

---
+++ CurrencyConverterDynamic/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace CurrencyConverterDynamic
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.inputBoxLabel = new System.Windows.Forms.Label();
+            this.usdRadioButton = new System.Windows.Forms.RadioButton();
+            this.eurRadioButton = new System.Windows.Forms.RadioButton();
+            this.jpyRadioButton = new System.Windows.Forms.RadioButton();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(82, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(293, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Currency Converter";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(129, 104);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(194, 23);
+            this.inputBox.TabIndex = 1;
+            this.inputBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // inputBoxLabel
+            // 
+            this.inputBoxLabel.AutoSize = true;
+            this.inputBoxLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.inputBoxLabel.Location = new System.Drawing.Point(79, 104);
+            this.inputBoxLabel.Name = "inputBoxLabel";
+            this.inputBoxLabel.Size = new System.Drawing.Size(37, 21);
+            this.inputBoxLabel.TabIndex = 2;
+            this.inputBoxLabel.Text = "IDR";
+            // 
+            // usdRadioButton
+            // 
+            this.usdRadioButton.AutoSize = true;
+            this.usdRadioButton.Location = new System.Drawing.Point(114, 185);
+            this.usdRadioButton.Name = "usdRadioButton";
+            this.usdRadioButton.Size = new System.Drawing.Size(47, 19);
+            this.usdRadioButton.TabIndex = 3;
+            this.usdRadioButton.TabStop = true;
+            this.usdRadioButton.Text = "USD";
+            this.usdRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // eurRadioButton
+            // 
+            this.eurRadioButton.AutoSize = true;
+            this.eurRadioButton.Location = new System.Drawing.Point(207, 185);
+            this.eurRadioButton.Name = "eurRadioButton";
+            this.eurRadioButton.Size = new System.Drawing.Size(46, 19);
+            this.eurRadioButton.TabIndex = 4;
+            this.eurRadioButton.TabStop = true;
+            this.eurRadioButton.Text = "EUR";
+            this.eurRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // jpyRadioButton
+            // 
+            this.jpyRadioButton.AutoSize = true;
+            this.jpyRadioButton.Location = new System.Drawing.Point(297, 185);
+            this.jpyRadioButton.Name = "jpyRadioButton";
+            this.jpyRadioButton.Size = new System.Drawing.Size(43, 19);
+            this.jpyRadioButton.TabIndex = 5;
+            this.jpyRadioButton.TabStop = true;
+            this.jpyRadioButton.Text = "JPY";
+            this.jpyRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(158, 239);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(127, 32);
+            this.convertButton.TabIndex = 6;
+            this.convertButton.Text = "CONVERT!";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // resultBox
+            // 
+            this.resultBox.Location = new System.Drawing.Point(129, 298);
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(194, 23);
+            this.resultBox.TabIndex = 7;
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.result.Location = new System.Drawing.Point(59, 298);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(65, 21);
+            this.result.TabIndex = 8;
+            this.result.Text = "Result :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 360);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.jpyRadioButton);
+            this.Controls.Add(this.eurRadioButton);
+            this.Controls.Add(this.usdRadioButton);
+            this.Controls.Add(this.inputBoxLabel);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Label inputBoxLabel;
+        private System.Windows.Forms.RadioButton usdRadioButton;
+        private System.Windows.Forms.RadioButton eurRadioButton;
+        private System.Windows.Forms.RadioButton jpyRadioButton;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.TextBox resultBox;
+        private System.Windows.Forms.Label result;
+    }
+}
+
---
+++ CurrencyConverterDynamic/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace CurrencyConverterDynamic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private double getRate(string fromCurrency, string toCurrency)
+        {
+            var json = "";
+            string rate = "";
+            try
+            {
+                string url = string.Format("https://free.currconv.com/api/v7/convert?q={0}_{1}&compact=ultra&apiKey=0c68165c1e1edd98f429", fromCurrency.ToUpper(), toCurrency.ToUpper());
+                string key = string.Format("{0}_{1}", fromCurrency.ToUpper(), toCurrency.ToUpper());
+
+                json = new WebClient().DownloadString(url);
+                dynamic stuff = JsonConvert.DeserializeObject(json);
+                rate = stuff[key];
+            }
+            catch
+            {
+                rate = "0";
+            }
+
+            return double.Parse(rate);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            if (usdRadioButton.Checked == true)
+            {
+                //radioButtonLabel.Text = "Choose One!";
+                //radioButtonLabel.ForeColor = Color.Black;
+
+                if (string.IsNullOrEmpty(inputBox.Text))
+                {
+                    inputBoxLabel.Text = "Insert number in IDR!";
+                    inputBoxLabel.ForeColor = Color.Red;
+                }
+                else
+                {
+                    inputBoxLabel.Text = "IDR";
+                    inputBoxLabel.ForeColor = Color.Black;
+
+                    double rate = getRate(inputBoxLabel.Text, usdRadioButton.Text);
+                    double coba = double.Parse(inputBox.Text);
+                    double output = rate*coba;
+
+                    resultBox.Text = output.ToString();
+                    //resultBoxLabel.Text = usdRadioButton.Text;
+                }
+            }
+
+            else if (jpyRadioButton.Checked == true)
+            {
+                //radioButtonLabel.Text = "Choose One!";
+                //radioButtonLabel.ForeColor = Color.Black;
+
+                if (string.IsNullOrEmpty(inputBox.Text))
+                {
+                    inputBoxLabel.Text = "Insert number in IDR!";
+                    inputBoxLabel.ForeColor = Color.Red;
+                }
+                else
+                {
+                    inputBoxLabel.Text = "IDR";
+                    inputBoxLabel.ForeColor = Color.Black;
+
+                    double rate = getRate(inputBoxLabel.Text, jpyRadioButton.Text);
+                    double output = double.Parse(inputBox.Text) * rate;
+
+                    resultBox.Text = output.ToString();
+                  //  resultBoxLabel.Text = jpyRadioButton.Text;
+                }
+            }
+            else if (eurRadioButton.Checked == true)
+            {
+                //radioButtonLabel.Text = "Choose One!";
+                //radioButtonLabel.ForeColor = Color.Black;
+
+                if (string.IsNullOrEmpty(inputBox.Text))
+                {
+                    inputBoxLabel.Text = "Insert number in IDR!";
+                    inputBoxLabel.ForeColor = Color.Red;
+                }
+                else
+                {
+                    inputBoxLabel.Text = "IDR";
+                    inputBoxLabel.ForeColor = Color.Black;
+
+                    double rate = getRate(inputBoxLabel.Text, eurRadioButton.Text);
+                    double output = double.Parse(inputBox.Text) * rate;
+
+                    resultBox.Text = output.ToString();
+                  //  resultBoxLabel.Text = eurRadioButton.Text;
+                }
+            }
+
+            else
+            {
+                //radioButtonLabel.Text = "Select one, please!";
+                //radioButtonLabel.ForeColor = Color.Red;
+            }
+
+        }
+    }
+}
